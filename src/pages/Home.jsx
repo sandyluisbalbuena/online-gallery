82aee94a7372f8138c5d5db85a06d2c40263d6aa
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-07-10 23:55:11 +0800

    wew

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
+import { getStorage, ref, listAll, getDownloadURL, uploadBytes, getMetadata } from 'firebase/storage';
 
 const Home = () => {
   const [imageUrls, setImageUrls] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
     // Initialize Firebase storage
@@ -27,23 +28,76 @@ const Home = () => {
       });
   }, []);
 
+  const handleImageUpload = () => {
+    Swal.fire({
+      title: 'Upload Image',
+      text: 'Please select an image file:',
+      input: 'file',
+      inputAttributes: {
+        accept: 'image/*'
+      },
+      showCancelButton: true,
+      confirmButtonText: 'Upload',
+      showLoaderOnConfirm: true,
+      preConfirm: (file) => {
+        if (file) {
+          const storage = getStorage();
+          const imagesRef = ref(storage, `img/${file.name}`);
+
+          return uploadBytes(imagesRef, file)
+            .then(() => {
+              return getMetadata(imagesRef)
+                .then(() => {
+                  return getDownloadURL(imagesRef)
+                    .then((url) => {
+                      setImageUrls((prevUrls) => [...prevUrls, url]);
+                      return 'Upload successful!';
+                    })
+                    .catch((error) => {
+                      throw new Error('Error getting download URL');
+                    });
+                })
+                .catch((error) => {
+                  throw new Error('Error getting metadata');
+                });
+            })
+            .catch((error) => {
+              throw new Error('Error uploading image');
+            });
+        } else {
+          throw new Error('No file selected');
+        }
+      },
+      allowOutsideClick: () => !Swal.isLoading()
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire('Success', result.value, 'success');
+      }
+    });
+  };
+
   return (
     <main className='container'>
-
       <section className='row mt-5'></section>
 
+      <div className='btn rounded upload-btn' onClick={handleImageUpload}>
+        <i className='fas fa-plus'></i>
+      </div>
+
       <section className='row'>
-
-      {imageUrls.map((imageUrl, index) => (
-        <div className='col-12 col-md-2'  key={index}>
-          <img className='rounded my-4' src={imageUrl} alt={`Image ${index}`} width='100%' height='90%'/>
-        </div>
-      ))}
-
+        {imageUrls.map((imageUrl, index) => (
+          <div className='col-12 col-md-2' key={index}>
+            <img
+              className='rounded my-4'
+              src={imageUrl}
+              alt={`Image ${index}`}
+              width='100%'
+              height='90%'
+            />
+          </div>
+        ))}
       </section>
-
     </main>
-    
   );
 };
 
