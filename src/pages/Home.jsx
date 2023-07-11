a5b51e499a5d5bd0d26d29ae74152458246d4569
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-07-11 19:11:34 +0800

    wew

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { getStorage, ref, listAll, getDownloadURL, uploadBytes, getMetadata } from 'firebase/storage';
+import { createCanvas, loadImage } from 'canvas';
 
 const Home = () => {
   const [imageUrls, setImageUrls] = useState([]);
-  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
     // Initialize Firebase storage
     const storage = getStorage();
+
+    getDataOneCard();
 
     // Create a reference to the 'img' folder in your Firebase storage bucket
     const imagesRef = ref(storage, 'img');
@@ -27,6 +29,105 @@ const Home = () => {
         console.log('Error fetching images:', error);
       });
   }, []);
+
+  const getDataOneCard = (Id) => {
+		axios.get(`https://pok3mon.online/api/users`)
+		.then(response => {
+			console.log(response.data);
+		})
+		.catch(error => console.error('On get one pokemon card error', error))
+		.then(() => { 
+	
+		})
+	}
+
+  const handleImageClick = (imageUrl, imgId, src) => {
+
+    var img = document.getElementById(imgId);
+
+    Swal.fire({
+      title: 'Select Filter',
+      html: `<div><select id="filter-select" class="swal2-input"><option value="grayscale">Grayscale</option><option value="sepia">Sepia</option><option value="blur">Blur</option></select></div>
+        <div><img src="${src}" class="swal2-image" alt="Selected Image"></div>`,
+      showCancelButton: true,
+      confirmButtonText: 'Apply',
+      preConfirm: () => {
+        const filterSelect = document.getElementById('filter-select');
+        const selectedFilter = filterSelect.value;
+        applyFilter(selectedFilter, imageUrl);
+      },
+      didOpen: () => {
+        Swal.getPopup().querySelector('.swal2-image').style.maxWidth = '400px';
+        Swal.getPopup().querySelector('.swal2-image').style.maxHeight = '200px';
+        img.onload = function() {
+          pixelsJS.filterImg(img, "grayscale");
+        };
+      }
+    });
+    
+    
+  };
+
+  const applyFilter = async (filter, imageUrl) => {
+    const canvas = createCanvas();
+    const ctx = canvas.getContext('2d');
+    const img = await loadImage(imageUrl);
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+  
+    // Apply the filter based on the selected option
+    if (filter === 'grayscale') {
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const pixels = imageData.data;
+      for (let i = 0; i < pixels.length; i += 4) {
+        const r = pixels[i];
+        const g = pixels[i + 1];
+        const b = pixels[i + 2];
+        const gray = (r + g + b) / 3;
+        pixels[i] = pixels[i + 1] = pixels[i + 2] = gray;
+      }
+      ctx.putImageData(imageData, 0, 0);
+    } else if (filter === 'sepia') {
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const pixels = imageData.data;
+      for (let i = 0; i < pixels.length; i += 4) {
+        const r = pixels[i];
+        const g = pixels[i + 1];
+        const b = pixels[i + 2];
+        const newR = Math.min(255, r * 0.393 + g * 0.769 + b * 0.189);
+        const newG = Math.min(255, r * 0.349 + g * 0.686 + b * 0.168);
+        const newB = Math.min(255, r * 0.272 + g * 0.534 + b * 0.131);
+        pixels[i] = newR;
+        pixels[i + 1] = newG;
+        pixels[i + 2] = newB;
+      }
+      ctx.putImageData(imageData, 0, 0);
+    } else if (filter === 'blur') {
+      ctx.filter = 'blur(5px)';
+    }
+  
+    const filteredImageURL = canvas.toDataURL();
+  
+    // Save the filtered image to Firebase
+    const storage = getStorage();
+    const imagesRef = ref(storage, 'img');
+    const filteredImageRef = ref(imagesRef, `filtered_${Date.now()}.png`);
+  
+    fetch(filteredImageURL)
+      .then((response) => response.blob())
+      .then((blob) => uploadBytes(filteredImageRef, blob))
+      .then(() => {
+        setImageUrls((prevUrls) => [...prevUrls, filteredImageURL]);
+        Swal.fire('Filter Applied', 'Filter applied successfully!', 'success');
+      })
+      .catch((error) => {
+        console.log('Error saving filtered image:', error);
+        Swal.fire('Error', 'An error occurred while saving the filtered image.', 'error');
+      });
+  };
+  
+  
 
   const handleImageUpload = () => {
     Swal.fire({
@@ -93,6 +194,8 @@ const Home = () => {
               alt={`Image ${index}`}
               width='100%'
               height='90%'
+              id={index}
+              onClick={() => handleImageClick(imageUrl, index, imageUrl)}
             />
           </div>
         ))}
